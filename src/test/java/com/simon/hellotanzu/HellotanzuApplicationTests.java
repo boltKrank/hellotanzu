700a6cf1ff98127f0a5084729bbace12c77d4265
Simon Anderson <simon.anderson111@gmail.com>
2022-02-22 07:07:28 +1100

    Initial commit

---
+++ src/test/java/com/simon/hellotanzu/HellotanzuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.hellotanzu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellotanzuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
